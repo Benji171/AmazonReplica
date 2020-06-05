99a259caa84bfc6c52333d6037bc4d01649873ea
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2020-06-05 14:10:33 -0400

    React App Setup/React Router Install

---
+++ frontend/src/data.js
@@ -0,0 +1,31 @@
+export default {
+    products : [
+        {
+        name: "Air Max 90's",
+        catagory: "Shoes",
+        image: "/images/d1.jpg",
+        price: 675,
+        brand: "Nike",
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: "Air Max 90's",
+        catagory: "Shoes",
+        image: "/images/d1.jpg",
+        price: 180,
+        brand: "Nike",
+        rating: 2.5,
+        numReviews: 10
+    },
+    {
+        name: "Air Max 90's",
+        catagory: "Jewelry",
+        image: "/images/d1.jpg",
+        price: 87,
+        brand: "Nike",
+        rating: 3.5,
+        numReviews: 10
+    },
+]
+}
